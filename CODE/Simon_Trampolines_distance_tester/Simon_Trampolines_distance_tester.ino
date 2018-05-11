4e48eb44882cfc2d30c068b0d848a900456865d0
Pete Lewis <plewis@sparkfun.com>
2018-05-11 15:47:24 -0600

    distance sensor testing tweaks
    Moved single range read to a function. Tried this out on all 4 ethernet ports and I'm getting good readings. Wahoo!

--- CODE/Simon_Trampolines_distance_tester/Simon_Trampolines_distance_tester.ino
+++ CODE/Simon_Trampolines_distance_tester/Simon_Trampolines_distance_tester.ino
@@ -11,11 +11,13 @@ Original code improvements to the Ping sketch sourced from Trollmaker.com
 Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
 */
 
-#define trigPin 13
-#define echoPin 12
-#define echoPin2 10
+#define trigPin 12
+#define echoPin 2
+#define echoPin2 3
+#define echoPin3 4
+#define echoPin4 8
 
-long duration, distance, duration2;
+long duration, duration2, duration3, duration4;
 
 boolean arm1, arm2;
 int threshold_up = 1000;
@@ -24,41 +26,46 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(echoPin2, INPUT);
+  pinMode(echoPin3, INPUT);
+  pinMode(echoPin4, INPUT);
 }
 
 void loop() {
 
 //  unsigned long start = micros();
 
+  duration = readDistance(echoPin);
+  duration2 = readDistance(echoPin2);
+  duration3 = readDistance(echoPin3);
+  duration4 = readDistance(echoPin4);
+
+    Serial.print(duration);
+    Serial.print(" \t");
+    Serial.print(duration2);
+    Serial.print(" \t");
+    Serial.print(duration3);
+    Serial.print(" \t");
+    Serial.println(duration4);
+
+//    if(duration > threshold_up) arm1 = true;
+//    if(duration2 > threshold_up) arm2 = true;
+//
+//    if((duration < threshold_up) && arm1)
+//    {
+//      Serial.println("\t\t\ttrigger 1");
+//      arm1 = false;
+//    }
+    delay(40);
+}
+
+long readDistance(int pin)
+{
   digitalWrite(trigPin, LOW);  
   delayMicroseconds(2); 
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10); 
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH, 8000);
-  
+  return pulseIn(pin, HIGH, 8000);
   delay(5);
-
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(1000); - Removed this line
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPin, LOW);
-  duration2 = pulseIn(echoPin2, HIGH, 8000);  
-
-  
-    Serial.print(duration);
-    Serial.print(" \t");
-    Serial.println(duration2);
-
-    if(duration > threshold_up) arm1 = true;
-    if(duration2 > threshold_up) arm2 = true;
-
-    if((duration < threshold_up) && arm1)
-    {
-      Serial.println("\t\t\ttrigger 1");
-      arm1 = false;
-    }
-    delay(40);
 }
+
